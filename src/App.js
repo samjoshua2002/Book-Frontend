6b2607bf5b7ff53d2574b42eee30cdc1dc6c5fca
Sam Joshua P <samjoshuacs2002@gmail.com>
2024-11-29 23:54:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Form from './components/Form';
+import Bookdetails from './components/Bookdetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex flex-col md:flex-row">
+      {/* Form Section */}
+      <div className="w-full md:w-1/2 p-4 bg-white ">
+        <Form />
+      </div>
+
+      {/* Bookdetails Section */}
+      <div className="w-full md:w-1/2 p-4 bg-white shadow-md">
+        <Bookdetails />
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
