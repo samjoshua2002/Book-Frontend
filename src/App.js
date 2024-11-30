f399c7840b0d42eb78cbaa484a267869f7d7b44d
Sam Joshua P <samjoshuacs2002@gmail.com>
2024-11-30 08:34:18 +0530

    fch

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Bookdetails from './components/Bookdetails';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 flex flex-col md:flex-row">
-      {/* Form Section */}
+      
       <div className="w-full md:w-1/2 p-4 bg-white ">
         <Form />
         
       </div>
 
-      {/* Bookdetails Section */}
+     
       <div className="w-full md:w-1/2 p-4 bg-white shadow-md">
         <Bookdetails />
       </div>
